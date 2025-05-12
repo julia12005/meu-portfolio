b402517ae8b6b297bf68fb5285c304eba47de7c5
julia12005 <julia.p.b@sempreceub.com>
2025-05-11 22:54:44 -0300

    Update DistanciaEntreDoisPontosNoPlanoCartesiano.py

--- projetos-pessoais/distancia-de-dois-pontos-cartesiano/DistanciaEntreDoisPontosNoPlanoCartesiano.py
+++ projetos-pessoais/distancia-de-dois-pontos-cartesiano/DistanciaEntreDoisPontosNoPlanoCartesiano.py
@@ -11,40 +11,4 @@ if x1==x2 and y1==y2:
     print("Os dois pontos são iguais, a distância é 0.")
 else:
     distancia=math.sqrt((x2-x1)**2+(y2-y1)**2)
-    print(f"A distância entre os dois pontos é:{distancia:.2f}.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(f"A distância entre os dois pontos é:{distancia:.2f}.
